95290c08ccc632d85eb6918c24b2c4926e1baec8
Peppe289 <gsperanza204@gmail.com>
2022-10-17 23:57:44 +0200

    add read system bash output

--- app/src/main/java/com/rave/tweak/MainActivity.kt
+++ app/src/main/java/com/rave/tweak/MainActivity.kt
@@ -1,16 +1,22 @@
 package com.rave.tweak
 
 import android.os.Bundle
-import android.widget.Switch
-import android.widget.Toast
+//import android.widget.Switch
+//import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.rave.tweak.databinding.ActivityMainBinding
-import android.widget.Button;
+import android.widget.Button
+import android.widget.TextView
+import com.jaredrummler.ktsh.Shell
 
 class MainActivity : AppCompatActivity() {
+/*
+    object StaticDef {
+        const val kernelver = "/proc/version"
+    } */
 
     private lateinit var binding: ActivityMainBinding
 
@@ -23,13 +29,18 @@ class MainActivity : AppCompatActivity() {
         /*val root = */
         Runtime.getRuntime().exec("su")
 
-        var balancedTweak = findViewById<Button>(R.id.button_id);
-
+        //val inputString = File("/proc/version").reader().use { it.readText() }
+        val shell = Shell("sh")
+        val inputString = shell.run("uname -r").stdout()
+        var balancedTweak = findViewById<Button>(R.id.button_id)
+/*
         balancedTweak?.setOnClickListener()
         {
-            //Toast.makeText(this@MainActivity, "Tapped", Toast.LENGTH_LONG).show()
 
-        }
+        } */
+
+        val textView: TextView = findViewById<TextView>(R.id.textView)
+        textView.text = inputString
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         // Passing each menu ID as a set of Ids because each
