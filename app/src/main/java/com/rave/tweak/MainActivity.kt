5c78650d96890471e592ba62ff2aca92b5237390
Peppe289 <gsperanza204@gmail.com>
2022-10-26 23:07:14 +0200

    soft update

--- app/src/main/java/com/rave/tweak/MainActivity.kt
+++ app/src/main/java/com/rave/tweak/MainActivity.kt
@@ -12,17 +12,67 @@ import android.widget.Button
 import android.widget.TextView
 import com.jaredrummler.ktsh.Shell
 
+
+/*
+// for dynamic managment. run in background. update later.
+credit: https://stackoverflow.com/a/34573169
+
+android studio convert this code in kotlin:
+
+class YourService : Service() {
+    @Nullable
+    fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+
+        // do your jobs here
+        startForeground()
+        return super.onStartCommand(intent, flags, startId)
+    }
+
+    private fun startForeground() {
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            this, 0,
+            notificationIntent, 0
+        )
+        startForeground(
+            NOTIF_ID, NotificationCompat.Builder(
+                this,
+                NOTIF_CHANNEL_ID
+            ) // don't forget create a notification channel first
+                .setOngoing(true)
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText("Service is running background")
+                .setContentIntent(pendingIntent)
+                .build()
+        )
+    }
+
+    companion object {
+        private const val NOTIF_ID = 1
+        private const val NOTIF_CHANNEL_ID = "Channel_Id"
+    }
+}
+*/
+
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
     object StaticDef {
         const val IOERROR = (-1)
+        const val primaryCluster = "/sys/devices/system/cpu/cpufreq/policy0/"
+        const val vfs_cache_pressure = "/proc/sys/vm/vfs_cache_pressure"
     }
 
-    fun write_node (patch: String, input: Int): Int {
+    fun write_node (patch: String, input: String): Int {
         try {
-            Shell("su").run(patch)
+            val command = StringBuilder().append("echo ").append(input).append(" > ").append(patch).toString()
+            Shell("su").run(command)
         } catch (e: NumberFormatException) {
             return StaticDef.IOERROR
         }
@@ -36,12 +86,21 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+
         // request root permission
         Runtime.getRuntime().exec("su")
 
         // take kernel version with uname -r
         val shell = Shell("sh")
         val inputString = shell.run("uname -r").stdout()
+
+        // upload all governor name in built kernel
+        val governorList = shell.run("cat " +
+                "/sys/devices/system/cpu/cpufreq/policy0/scaling_available_governors")
+                                            .stdout().split(" ").toTypedArray()
+
+        // upload available cluster
+        //var cluster = ""
 
         /*
          * Here button for tweak.
@@ -55,7 +114,29 @@ class MainActivity : AppCompatActivity() {
         val powersave = findViewById<Button>(R.id.powersave)
 
         performance?.setOnClickListener() {
+            // use better governor for performance.
+            // if chose performance governor maybe u can see throttling!!!
+            var governor = "none"
+            val VFSCachePressureValue = "60"
 
+
+            // chose governor:
+            for (i in governorList) {
+                if (i == "performance") {
+                    governor = i
+                    // prefer performance
+                    break
+                } else if (i == "schedutil")
+                    governor = i
+                else if (i == "interactive")
+                    governor = i
+            }
+
+            if (governor != "none") // if find governor, set it
+                write_node(StringBuilder().append(StaticDef.primaryCluster)
+                                            .append("scaling_governor").toString(), governor)
+
+            write_node(StaticDef.vfs_cache_pressure,VFSCachePressureValue)
         }
 
         balanced?.setOnClickListener() {
